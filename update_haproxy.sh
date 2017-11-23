be8a73225470f13bb7d8b3268fe47ae7a27cf2aa
Roberto J Rojas <robertojrojas@gmail.com>
2017-11-22 22:47:43 -0500

    updates haproxy

--- update_haproxy.sh
+++ update_haproxy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-alias ansible-p="PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s' ansible-playbook "
 KUARD_PORT=$(vagrant ssh controller1 -c 'kubectl get svc kuard --no-headers' 2>&1 | head -n 1 | awk '{print $4}' | awk -F: '{print $2}' | cut -d/ -f1)
-ansible-p -i k8s_vms_inventory k8s_the_hard_way/update_haproxy.yml -e "kuard_port=${KUARD_PORT}"
+
+PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false \
+ ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s' \
+ ansible-playbook -i k8s_vms_inventory k8s_the_hard_way/update_haproxy.yml -e "kuard_port=${KUARD_PORT}"
